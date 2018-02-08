8d1f69bcdc09183bfd9cc3595c989352ee7252c5
Emory Merryman <emory.merryman@gmail.com>
2018-02-08 15:45:53 +0000

    fix docker script

--- root/bin/docker.sh
+++ root/bin/docker.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
 
+sudo \
+    --preserve-env \
+    /usr/bin/docker \
+    container \
+    run \
+    --interactive \
+    --tty \
+    --rm \
+    --label expiry=$(($(date +%s)+60*60*24*7)) \
+    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    --volume docker:/srv/docker \
+    --workdir $(pwd) \
+    docker:17.12.0 \
+        "${@}"
