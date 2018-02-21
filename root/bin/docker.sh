3f84f84adfd5df17f998e7e50e2dd4d8efc2b311
Emory Merryman <emory.merryman@gmail.com>
2018-02-21 12:49:35 +0000

    fix docker

--- root/bin/docker.sh
+++ root/bin/docker.sh
@@ -6,6 +6,7 @@
     --interactive \
     --tty \
     --rm \
+    --mount type=bind,source=/srv/docker,destination=/srv/docker,readonly=false \
     $(compgen -v | while read ENV; do echo --env ${ENV}; done) \
     --workdir $(pwd) \
     docker:${DOCKER_SEMVER}-ce \
