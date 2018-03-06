5189c26713642e9f915c69e249eaddc2d2e19019
Emory Merryman <emory.merryman@gmail.com>
2018-03-06 02:03:43 +0000

    better cleanup

--- extension/cleanup.sh
+++ extension/cleanup.sh
@@ -1,24 +1,19 @@
 #!/bin/sh
 
-ls -1 /srv/docker/containers | while read FILE
+ls -1 /home/user/workspace/docker/containers | while read FILE
 do
-    sudo /usr/bin/docker container stop $(cat /srv/docker/containers/${FILE}) &&
-        sudo /usr/bin/docker container rm --volumes $(cat /srv/docker/containers/${FILE}) &&
-        rm -f /srv/docker/${FILE}
+    sudo /usr/bin/docker container stop $(cat /home/user/workspace/docker/containers/${FILE}) &&
+        sudo /usr/bin/docker container rm --volumes $(cat /home/user/workspace/docker/containers/${FILE}) &&
+        rm -f /home/user/workspace/docker/${FILE}
 done &&
-    ls -1 /srv/docker/images | while read FILE
+    ls -1 /home/user/workspace/docker/networks | while read FILE
     do
-        sudo /usr/bin/docker image rm $(cat /srv/docker/images/${FILE}) &&
-            rm -f /srv/docker/${FILE}
+        sudo /usr/bin/docker network rm $(cat /home/user/workspace/docker/networks/${FILE}) &&
+            rm -f /home/user/workspace/docker/${FILE}
     done &&
-    ls -1 /srv/docker/networks | while read FILE
+    ls -1 /home/user/workspace/docker/volumes | while read FILE
     do
-        sudo /usr/bin/docker network rm $(cat /srv/docker/networks/${FILE}) &&
-            rm -f /srv/docker/${FILE}
-    done &&
-    ls -1 /srv/docker/volumes | while read FILE
-    do
-        sudo /usr/bin/docker volume rm $(cat /srv/docker/volumes/${FILE}) &&
-            rm -f /srv/docker/${FILE}
+        sudo /usr/bin/docker volume rm $(cat /home/user/workspace/docker/volumes/${FILE}) &&
+            rm -f /home/user/workspace/docker/${FILE}
     done
         
