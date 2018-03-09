0206c111ff18cb3ba30c626cd22f47d2e360bb40
Emory Merryman <emory.merryman@gmail.com>
2018-03-09 21:05:48 +0000

    preparation commit

--- extension/init.root.sh
+++ extension/init.root.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-if [ ! -z "${TARGET_UID}" ]
-then
-    usermod -u ${TARGET_UID} user
-fi
+dnf update --assumeyes &&
+    if [ ! -z "${TARGET_UID}" ]
+    then
+        usermod -u ${TARGET_UID} user
+    fi
