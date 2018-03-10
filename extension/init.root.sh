3e745037a9fe461e8a38d7b1a256e8a36251d870
Emory Merryman <emory.merryman@gmail.com>
2018-03-10 12:03:12 +0000

    do not dnf update ... it takes too much time

--- extension/init.root.sh
+++ extension/init.root.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-dnf update --assumeyes &&
-    if [ ! -z "${TARGET_UID}" ]
-    then
-        usermod -u ${TARGET_UID} user
-    fi
+if [ ! -z "${TARGET_UID}" ]
+then
+    usermod -u ${TARGET_UID} user
+fi
