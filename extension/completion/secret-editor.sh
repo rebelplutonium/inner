2976f90d82fe41f8bbf53367ef6db0b7bb9f7ec9
Emory Merryman <emory.merryman@gmail.com>
2018-07-30 20:28:21 +0000

    use newer base cloud9 image

--- extension/completion/secret-editor.sh
+++ extension/completion/secret-editor.sh
@@ -5,7 +5,7 @@ _UseGetOpt_secret_editor(){
         COMPREPLY=() &&
         CUR=${COMP_WORDS[COMP_CWORD]} &&
         case ${CUR} in
-            -)
+            -*)
                 COMPREPLY=($(compgen -W "--project-name --cloud9-port --gpg-secret-key --gpg-owner-trust --gpg2-secret-key --gpg2-owner-trust --origin-host --origin-port --origin-id-rsa --origin-organization --origin-repository --committer-name --commiter-email --read-write --read-only" -- ${CUR}))
         esac
 } &&
